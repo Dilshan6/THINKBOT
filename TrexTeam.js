8a39dde5dd31a51649d0840a9e347163a2234836
Dilshan6 <115965308+Dilshan6@users.noreply.github.com>
2022-10-17 12:52:13 +0530

    Create TrexTeam.js

---
+++ TrexTeam.js
@@ -0,0 +1,5 @@
+module.exports = {
+    THink_OWNER: '94776233007', // Think Team - Think Owner
+    THink_TEAM: '94776233007,94765808274,', // Team - Think TEAM
+    THink_OWNER: '94776233007' // Dilshan theekshana - Think Owner --> For The Friendship And co Owner in Think 
+}
